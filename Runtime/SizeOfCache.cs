c104d0a207536ef951113c464bbfa60dcea51424
Torgo13 <t.d.walker42@gmail.com>
2025-08-03 20:31:48 +0100

    Allow SizeOfCache to be used in generic functions without "where T : struct"

--- Runtime/SizeOfCache.cs
+++ Runtime/SizeOfCache.cs
@@ -7,7 +7,7 @@ namespace UnityExtensions
     /// Stores the marshalled size of a struct.
     /// </summary>
     /// <typeparam name="T">The type of struct to get the size of.</typeparam>
-    public class SizeOfCache<T> where T : struct
+    public class SizeOfCache<T>
     {
         //https://github.com/Unity-Technologies/UnityLiveCapture/blob/4.0.1/Packages/com.unity.live-capture/Networking/Utilities/SizeOfCache.cs
         #region Unity.LiveCapture.Networking
@@ -18,8 +18,16 @@ namespace UnityExtensions
 
         static SizeOfCache()
         {
-            var t = typeof(T).IsEnum ? Enum.GetUnderlyingType(typeof(T)) : typeof(T);
-            Size = Marshal.SizeOf(t);
+            Type t = typeof(T);
+            bool isEnum = t.IsEnum;
+            if (!t.IsValueType && !isEnum)
+            {
+                Size = -1;
+                return;
+            }
+
+            t = isEnum ? Enum.GetUnderlyingType(t) : t;
+            Size = Unity.Collections.LowLevel.Unsafe.UnsafeUtility.SizeOf(t);
         }
         #endregion // Unity.LiveCapture.Networking
     }
