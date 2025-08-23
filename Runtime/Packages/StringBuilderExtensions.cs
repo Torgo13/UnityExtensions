172d7378ebd18c016565d5c19e017b94b62ea885
Torgo13 <t.d.walker42@gmail.com>
2025-08-23 13:09:51 +0100

    Added UnsafeListExtensionsTests
    Moved StringBuilder Remove and Replace extensions and its tests to a separate package

--- Runtime/Packages/StringBuilderExtensions.cs
+++ Runtime/Packages/StringBuilderExtensions.cs
@@ -33,61 +33,6 @@ namespace UnityExtensions.Packages
             }
         }
         #endregion // UnityEditor.ShaderGraph
-        
-        /// <summary>
-        /// Removes all occurrences of specified characters from <see cref="StringBuilder"/>.
-        /// </summary>
-        /// <param name="sb">A <see cref="StringBuilder"/> to remove from.</param>
-        /// <param name="removeChar">A Unicode character to remove.</param>
-        /// <returns>
-        /// Returns <see cref="StringBuilder"/> without specified Unicode characters.
-        /// </returns>
-        /// <exception cref="ArgumentNullException"><paramref name="sb"/> is null.</exception>
-        public static StringBuilder Remove(this StringBuilder sb, char removeChar)
-        {
-            if (sb == null)
-                throw new ArgumentNullException(nameof(sb));
-
-            for (int i = 0; i < sb.Length;)
-            {
-                if (removeChar == sb[i])
-                    _ = sb.Remove(i, 1);
-                else
-                    i++;
-            }
-
-            return sb;
-        }
-        
-        /// <summary>
-        /// Discard StringComparison to keep compatibility with string.Replace
-        /// </summary>
-        public static StringBuilder Replace(this StringBuilder stringBuilder, string oldValue, string newValue,
-#pragma warning disable IDE0060 // Remove unused parameter
-            StringComparison comparisonType)
-#pragma warning restore IDE0060 // Remove unused parameter
-        {
-            return stringBuilder.Replace(oldValue, newValue);
-        }
-        
-        public static StringBuilder Replace(this StringBuilder stringBuilder, string oldValue, int newValue,
-            bool ignoreCase = false)
-        {
-            int oldValueLength = oldValue?.Length ?? 0;
-            if (oldValueLength == 0)
-                return stringBuilder;
-            
-            int index = stringBuilder.IndexOf(oldValue, ignoreCase);
-            while (index != -1)
-            {
-                _ = stringBuilder.Remove(index, oldValueLength);
-                _ = stringBuilder.Insert(index, newValue);
-                
-                index = stringBuilder.IndexOf(oldValue, ignoreCase);
-            }
-
-            return stringBuilder;
-        }
 
         public static StringBuilder Append(this StringBuilder stringBuilder, string value, int startIndex)
         {
@@ -96,33 +41,10 @@ namespace UnityExtensions.Packages
 
             return stringBuilder.Append(value, startIndex, length);
         }
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static StringBuilder EnsureRoom(this StringBuilder stringBuilder, int room)
-        {
-            _ = stringBuilder.EnsureCapacity(stringBuilder.Length + room);
-            return stringBuilder;
-        }
         
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         static bool IgnoreCase(StringComparison comparisonType = default)
         {
             return (int)comparisonType % 2 != 0;
-        }
-
-        public static bool Contains(this StringBuilder stringBuilder, string value, bool ignoreCase = false)
-        {
-            return stringBuilder.IndexOf(value, ignoreCase) >= 0;
-        }
-
-        public static bool Contains(this StringBuilder stringBuilder, string value, StringComparison comparisonType)
-        {
-            return stringBuilder.IndexOf(value, IgnoreCase(comparisonType)) >= 0;
-        }
-
-        public static bool Contains(this StringBuilder stringBuilder, char value)
-        {
-            return stringBuilder.IndexOf(value) >= 0;
         }
     }
 }
--- Tests/Editor/Unsafe/UnsafeListExtensionsTests.cs
+++ Tests/Editor/Unsafe/UnsafeListExtensionsTests.cs
@@ -1,7 +1,10 @@
+using System;
 using System.Collections.Generic;
 using NUnit.Framework;
 using Unity.Collections;
 using Unity.Collections.LowLevel.Unsafe;
+
+using NUnitAssert = NUnit.Framework.Assert;
 
 namespace UnityExtensions.Unsafe.Tests
 {
@@ -104,6 +107,379 @@ namespace UnityExtensions.Unsafe.Tests
             }
 
             unsafeList.Dispose();
+        }
+
+        private UnsafeList<int> _intDefault;               // default (not created)
+        private UnsafeList<int> _intEmptyCreated;          // created, capacity > 0, length == 0
+        private UnsafeList<int> _intSmall;                 // created, small capacity, length set by helper
+        private UnsafeList<MyStruct> _structList;          // created, for generic coverage
+
+        [SetUp]
+        public unsafe void SetUp()
+        {
+            // default (not created)
+            _intDefault = default;
+
+            // created (length 0)
+            _intEmptyCreated = new UnsafeList<int>(4, Allocator.Persistent);
+            _intEmptyCreated.Length = 0;
+
+            // small list with values 1, 2, 3
+            _intSmall = new UnsafeList<int>(4, Allocator.Persistent);
+            Populate(ref _intSmall, new[] { 1, 2, 3 });
+
+            // struct list with single element
+            _structList = new UnsafeList<MyStruct>(2, Allocator.Persistent);
+            Populate(ref _structList, new[] { new MyStruct { A = 7, B = 11 } });
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            DisposeIfCreated(ref _intEmptyCreated);
+            DisposeIfCreated(ref _intSmall);
+            DisposeIfCreated(ref _structList);
+            // _intDefault is not created
+        }
+
+        // ----- GetPtr -----
+
+        [Test]
+        public unsafe void GetPtr_ReturnsNull_OnDefaultNotCreated()
+        {
+            var p = _intDefault.GetPtr<int>();
+            NUnitAssert.AreEqual(IntPtr.Zero, (IntPtr)p);
+        }
+
+        [Test]
+        public unsafe void GetPtr_ReturnsNull_OnCreatedButEmpty()
+        {
+            var p = _intEmptyCreated.GetPtr<int>();
+            NUnitAssert.AreEqual(IntPtr.Zero, (IntPtr)p);
+        }
+
+        [Test]
+        public unsafe void GetPtr_ReturnsListPtr_OnNonEmpty()
+        {
+            var p = _intSmall.GetPtr<int>();
+            NUnitAssert.AreNotEqual(IntPtr.Zero, (IntPtr)p);
+            NUnitAssert.AreEqual(new IntPtr(_intSmall.Ptr), (IntPtr)p);
+        }
+
+        // ----- GetReadOnlyPtr -----
+
+        [Test]
+        public unsafe void GetReadOnlyPtr_ReturnsNull_OnDefaultNotCreated()
+        {
+            var p = _intDefault.GetReadOnlyPtr<int>();
+            NUnitAssert.AreEqual(IntPtr.Zero, (IntPtr)p);
+        }
+
+        [Test]
+        public unsafe void GetReadOnlyPtr_ReturnsNull_OnCreatedButEmpty()
+        {
+            var p = _intEmptyCreated.GetReadOnlyPtr<int>();
+            NUnitAssert.AreEqual(IntPtr.Zero, (IntPtr)p);
+        }
+
+        [Test]
+        public unsafe void GetReadOnlyPtr_ReturnsReadOnlyPtr_OnNonEmpty()
+        {
+            var p = _intSmall.GetReadOnlyPtr<int>();
+            NUnitAssert.AreNotEqual(IntPtr.Zero, (IntPtr)p);
+            NUnitAssert.AreEqual(new IntPtr(_intSmall.AsReadOnly().Ptr), (IntPtr)p);
+        }
+
+        // ----- GetIntPtr -----
+
+        [Test]
+        public void GetIntPtr_ReturnsZero_OnDefaultNotCreated()
+        {
+            var ip = _intDefault.GetIntPtr();
+            NUnitAssert.AreEqual(IntPtr.Zero, ip);
+        }
+
+        [Test]
+        public void GetIntPtr_ReturnsZero_OnCreatedButEmpty()
+        {
+            var ip = _intEmptyCreated.GetIntPtr();
+            NUnitAssert.AreEqual(IntPtr.Zero, ip);
+        }
+
+        [Test]
+        public unsafe void GetIntPtr_ReturnsListPtr_OnNonEmpty()
+        {
+            var ip = _intSmall.GetIntPtr();
+            NUnitAssert.AreNotEqual(IntPtr.Zero, ip);
+            NUnitAssert.AreEqual(new IntPtr(_intSmall.Ptr), ip);
+        }
+
+        // ----- GetReadOnlyIntPtr -----
+
+        [Test]
+        public void GetReadOnlyIntPtr_ReturnsZero_OnDefaultNotCreated()
+        {
+            var ip = _intDefault.GetReadOnlyIntPtr();
+            NUnitAssert.AreEqual(IntPtr.Zero, ip);
+        }
+
+        [Test]
+        public void GetReadOnlyIntPtr_ReturnsZero_OnCreatedButEmpty()
+        {
+            var ip = _intEmptyCreated.GetReadOnlyIntPtr();
+            NUnitAssert.AreEqual(IntPtr.Zero, ip);
+        }
+
+        [Test]
+        public unsafe void GetReadOnlyIntPtr_EqualsReadOnlyPtr_OnNonEmpty()
+        {
+            var ip = _intSmall.GetReadOnlyIntPtr();
+            NUnitAssert.AreNotEqual(IntPtr.Zero, ip);
+            NUnitAssert.AreEqual(new IntPtr(_intSmall.AsReadOnly().Ptr), ip);
+        }
+
+        // ----- EnsureCapacity -----
+
+        [Test]
+        public void EnsureCapacity_Throws_WhenNotCreated()
+        {
+            // UnityEngine.Assertions.Assert throws UnityEngine.Assertions.AssertionException
+            NUnitAssert.Throws<UnityEngine.Assertions.AssertionException>(() =>
+            {
+                UnsafeListExtensions.EnsureCapacity(ref _intDefault, 1);
+            });
+        }
+
+        [TestCase(0)]
+        [TestCase(-1)]
+        public void EnsureCapacity_Throws_WhenCapacityNotPositive(int requested)
+        {
+            var list = new UnsafeList<int>(4, Allocator.Persistent);
+            try
+            {
+                NUnitAssert.Throws<UnityEngine.Assertions.AssertionException>(() =>
+                {
+                    UnsafeListExtensions.EnsureCapacity(ref list, requested);
+                });
+            }
+            finally
+            {
+                if (list.IsCreated)
+                    list.Dispose();
+            }
+        }
+
+        [Test]
+        public void EnsureCapacity_Increases_WhenBelowRequested()
+        {
+            var list = new UnsafeList<int>(4, Allocator.Persistent)
+            {
+                0,
+                0,
+                0,
+                0
+            };
+            try
+            {
+                NUnitAssert.AreEqual(4, list.Length);
+                NUnitAssert.AreEqual(16, list.Capacity);
+                UnsafeListExtensions.EnsureCapacity(ref list, 32);
+                NUnitAssert.AreEqual(32, list.Capacity);
+            }
+            finally
+            {
+                if (list.IsCreated)
+                    list.Dispose();
+            }
+        }
+
+        [Test]
+        public void EnsureCapacity_NoChange_WhenAlreadyEnough()
+        {
+            var list = new UnsafeList<int>(16, Allocator.Persistent);
+            try
+            {
+                UnsafeListExtensions.EnsureCapacity(ref list, 10);
+                NUnitAssert.AreEqual(16, list.Capacity);
+            }
+            finally
+            {
+                if (list.IsCreated)
+                    list.Dispose();
+            }
+        }
+
+        // ----- EnsureRoom -----
+
+        [Test]
+        public void EnsureRoom_Throws_WhenNotCreated()
+        {
+            NUnitAssert.Throws<UnityEngine.Assertions.AssertionException>(() =>
+            {
+                UnsafeListExtensions.EnsureRoom(ref _intDefault, 1);
+            });
+        }
+
+        [TestCase(0)]
+        [TestCase(-1)]
+        public void EnsureRoom_Throws_WhenRoomNotPositive(int room)
+        {
+            var list = new UnsafeList<int>(4, Allocator.Persistent);
+            try
+            {
+                NUnitAssert.Throws<UnityEngine.Assertions.AssertionException>(() =>
+                {
+                    UnsafeListExtensions.EnsureRoom(ref list, room);
+                });
+            }
+            finally
+            {
+                if (list.IsCreated)
+                    list.Dispose();
+            }
+        }
+
+        [Test]
+        public unsafe void EnsureRoom_GrowsTo_LengthPlusRoom_WhenInsufficientCapacity()
+        {
+            var list = new UnsafeList<int>(4, Allocator.Persistent);
+            try
+            {
+                // length = 3, capacity = 4 -> room 5 requires capacity >= 8
+                Populate(ref list, new[] { 10, 20, 30 });
+                NUnitAssert.AreEqual(3, list.Length);
+                NUnitAssert.AreEqual(16, list.Capacity); // Minimum capacity is 16
+
+                UnsafeListExtensions.EnsureRoom(ref list, 5);
+                NUnitAssert.AreEqual(16, list.Capacity); // Minimum capacity is 16
+            }
+            finally
+            {
+                if (list.IsCreated)
+                    list.Dispose();
+            }
+        }
+
+        [Test]
+        public unsafe void EnsureRoom_NoChange_WhenCapacityAlreadySufficient()
+        {
+            var list = new UnsafeList<int>(10, Allocator.Persistent);
+            try
+            {
+                Populate(ref list, new[] { 10, 20, 30 }); // length = 3
+                UnsafeListExtensions.EnsureRoom(ref list, 2); // needs 5
+                NUnitAssert.AreEqual(16, list.Capacity); // Minimum capacity is 16
+            }
+            finally
+            {
+                if (list.IsCreated)
+                    list.Dispose();
+            }
+        }
+
+        // ----- AsSpan -----
+
+        [Test]
+        public void AsSpan_ReturnsEmpty_OnDefaultNotCreated()
+        {
+            var span = _intDefault.AsSpan();
+            NUnitAssert.AreEqual(0, span.Length);
+            NUnitAssert.IsTrue(span.IsEmpty);
+        }
+
+        [Test]
+        public void AsSpan_ReturnsEmpty_OnCreatedButEmpty()
+        {
+            var span = _intEmptyCreated.AsSpan();
+            NUnitAssert.AreEqual(0, span.Length);
+            NUnitAssert.IsTrue(span.IsEmpty);
+        }
+
+        [Test]
+        public unsafe void AsSpan_ExposesWritableView_BackedByListMemory()
+        {
+            var span = _intSmall.AsSpan();
+            NUnitAssert.AreEqual(_intSmall.Length, span.Length);
+            NUnitAssert.AreEqual(3, span.Length);
+            // mutate via span
+            span[1] = 99;
+
+            // verify underlying memory changed
+            var value = UnsafeUtility.ReadArrayElement<int>(_intSmall.Ptr, 1);
+            NUnitAssert.AreEqual(99, value);
+        }
+
+        [Test]
+        public unsafe void AsSpan_WorksWithStructs_WriteThroughUpdatesUnderlying()
+        {
+            var span = _structList.AsSpan();
+            NUnitAssert.AreEqual(1, span.Length);
+            var s = span[0];
+            NUnitAssert.AreEqual(7, s.A);
+            NUnitAssert.AreEqual(11, s.B);
+
+            // write through span
+            s.B = 42;
+            span[0] = s;
+
+            // verify via raw read
+            var raw = UnsafeUtility.ReadArrayElement<MyStruct>(_structList.Ptr, 0);
+            NUnitAssert.AreEqual(42, raw.B);
+        }
+
+        // ----- AsReadOnlySpan -----
+
+        [Test]
+        public void AsReadOnlySpan_ReturnsEmpty_OnDefaultNotCreated()
+        {
+            var ro = _intDefault.AsReadOnlySpan();
+            NUnitAssert.AreEqual(0, ro.Length);
+            NUnitAssert.IsTrue(ro.IsEmpty);
+        }
+
+        [Test]
+        public void AsReadOnlySpan_ReturnsEmpty_OnCreatedButEmpty()
+        {
+            var ro = _intEmptyCreated.AsReadOnlySpan();
+            NUnitAssert.AreEqual(0, ro.Length);
+            NUnitAssert.IsTrue(ro.IsEmpty);
+        }
+
+        [Test]
+        public void AsReadOnlySpan_ReflectsCurrentContent()
+        {
+            var ro = _intSmall.AsReadOnlySpan();
+            NUnitAssert.AreEqual(_intSmall.Length, ro.Length);
+            NUnitAssert.AreEqual(1, ro[0]);
+            NUnitAssert.AreEqual(2, ro[1]);
+            NUnitAssert.AreEqual(3, ro[2]);
+        }
+
+        // ----- Helper: populate / dispose -----
+
+        private static unsafe void Populate<T>(ref UnsafeList<T> list, T[] values) where T : unmanaged
+        {
+            if (!list.IsCreated)
+                list = new UnsafeList<T>(values.Length, Allocator.Persistent);
+
+            list.EnsureCapacity(values.Length);
+            list.Length = values.Length;
+
+            for (int i = 0; i < values.Length; i++)
+                UnsafeUtility.WriteArrayElement(list.Ptr, i, values[i]);
+        }
+
+        private static void DisposeIfCreated<T>(ref UnsafeList<T> list) where T : unmanaged
+        {
+            if (list.IsCreated)
+                list.Dispose();
+            list = default;
+        }
+
+        private struct MyStruct
+        {
+            public int A;
+            public int B;
         }
     }
 }
